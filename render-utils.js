c6d6d2f0a0e91b3674d626e9281e7582881e371d
James Keicher <jamesdkeicher@gmail.com>
2021-12-20 10:36:18 -0800

    Changed class of each dogs container to match rubric requirement. Changed function call to a load event listener on the window element to meet rubric requirememnts

--- render-utils.js
+++ render-utils.js
@@ -2,7 +2,7 @@ export const renderDogs = (dogs) => {
     const dogKennel = document.createElement('div');
     for (const dog of dogs) {
         const dogContainerEl = document.createElement('div');
-        dogContainerEl.classList.add('dog-container');
+        dogContainerEl.classList.add('dog-item');
         const dogNameEl = document.createElement('p');
         dogNameEl.classList.add('dog-p');
         const dogAgeEl = document.createElement('span');
