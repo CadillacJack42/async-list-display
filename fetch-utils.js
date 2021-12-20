13d16f18b3d21aa420e6ac6db26db30227516f31
James Keicher <jamesdkeicher@gmail.com>
2021-12-20 09:14:31 -0800

    Html Stripped, Supabase CDN Script tag added, fetch-utils created with SUPABASE keys

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,10 +1,4 @@
-// import functions and grab DOM elements
-
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MDAxOTY4MywiZXhwIjoxOTU1NTk1NjgzfQ.0WT-gqj-qvV0wYfg0QdblxbkS4J4rIq0wf8BI3R45yc';
 const SUPABASE_URL = 'https://cmewyjgphfnmytfmmpjy.supabase.co';
-// let state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
